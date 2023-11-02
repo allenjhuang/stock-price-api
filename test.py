e101b352664ca14d728b351ed063adb73654feba
unknown <kzhao32@cs.wisc.edu>
2023-11-02 10:52:42 -0500

    change backend from yahoofinance to twelvedata

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import server_function
+import main
 from datetime import datetime, timedelta
 import json
 from time import perf_counter
@@ -10,18 +10,18 @@ def test():
 
     # Daily
     start_time = perf_counter()
-    print(f'server_function.get_daily_stock_data = {server_function.get_daily_stock_data(requested_tickers)}\n')
+    print(f'main.get_daily_stock_data = {main.get_daily_stock_data(requested_tickers)}\n')
     end_time = perf_counter()
     print(f'took {end_time - start_time} seconds\n')
 
     # 1 day
     start_time = perf_counter()
-    print(f'"1d" = {server_function.get_time_since_stock_data(requested_tickers, "1d")}')
+    print(f'"1d" = {main.get_time_since_stock_data(requested_tickers, "1d")}')
     end_time = perf_counter()
     print(f'took {end_time - start_time} seconds\n')
 
     start_time = perf_counter()
-    print(f'1 = {server_function.get_time_since_stock_data(requested_tickers, int((datetime.now(tz=server_function.EST5EDT()).replace(hour=16, minute=0, second=0, microsecond=0) - timedelta(days=1)).timestamp()))}')
+    print(f'1 = {main.get_time_since_stock_data(requested_tickers, int((datetime.now(tz=main.EST5EDT()).replace(hour=16, minute=0, second=0, microsecond=0) - timedelta(days=1)).timestamp()))}')
     end_time = perf_counter()
     print(f'took {end_time - start_time} seconds\n')
 
@@ -30,81 +30,80 @@ def test():
 
     # 5 day
     start_time = perf_counter()
-    print(f'"5d" = {server_function.get_time_since_stock_data(requested_tickers, "5d")}')
+    print(f'"5d" = {main.get_time_since_stock_data(requested_tickers, "5d")}')
     end_time = perf_counter()
     print(f'took {end_time - start_time} seconds\n')
 
     start_time = perf_counter()
-    print(f'5 = {server_function.get_time_since_stock_data(requested_tickers, int((datetime.now(tz=server_function.EST5EDT()).replace(hour=16, minute=0, second=0, microsecond=0) - timedelta(days=5)).timestamp()))}')
+    print(f'5 = {main.get_time_since_stock_data(requested_tickers, int((datetime.now(tz=main.EST5EDT()).replace(hour=16, minute=0, second=0, microsecond=0) - timedelta(days=5)).timestamp()))}')
     end_time = perf_counter()
     print(f'took {end_time - start_time} seconds\n')
 
     # 1 month
     start_time = perf_counter()
-    print(f'"1mo" = {server_function.get_time_since_stock_data(requested_tickers, "1mo")}')
+    print(f'"1mo" = {main.get_time_since_stock_data(requested_tickers, "1mo")}')
     end_time = perf_counter()
     print(f'took {end_time - start_time} seconds\n')
 
     start_time = perf_counter()
-    print(f'30 = {server_function.get_time_since_stock_data(requested_tickers, int((datetime.now(tz=server_function.EST5EDT()).replace(hour=16, minute=0, second=0, microsecond=0) - timedelta(days=30)).timestamp()))}')
+    print(f'30 = {main.get_time_since_stock_data(requested_tickers, int((datetime.now(tz=main.EST5EDT()).replace(hour=16, minute=0, second=0, microsecond=0) - timedelta(days=30)).timestamp()))}')
     end_time = perf_counter()
     print(f'took {end_time - start_time} seconds\n')
 
     # 6 month
     start_time = perf_counter()
-    print(f'"6mo" = {server_function.get_time_since_stock_data(requested_tickers, "6mo")}')
+    print(f'"6mo" = {main.get_time_since_stock_data(requested_tickers, "6mo")}')
     end_time = perf_counter()
     print(f'took {end_time - start_time} seconds\n')
 
     start_time = perf_counter()
-    print(f'180 = {server_function.get_time_since_stock_data(requested_tickers, int((datetime.now(tz=server_function.EST5EDT()).replace(hour=16, minute=0, second=0, microsecond=0) - timedelta(days=180)).timestamp()))}')
+    print(f'180 = {main.get_time_since_stock_data(requested_tickers, int((datetime.now(tz=main.EST5EDT()).replace(hour=16, minute=0, second=0, microsecond=0) - timedelta(days=180)).timestamp()))}')
     end_time = perf_counter()
     print(f'took {end_time - start_time} seconds\n')
 
     # 1 year
     start_time = perf_counter()
-    print(f'"1y" = {server_function.get_time_since_stock_data(requested_tickers, "1y")}')
+    print(f'"1y" = {main.get_time_since_stock_data(requested_tickers, "1y")}')
     end_time = perf_counter()
     print(f'took {end_time - start_time} seconds\n')
 
     start_time = perf_counter()
-    print(f'365 = {server_function.get_time_since_stock_data(requested_tickers, int((datetime.now(tz=server_function.EST5EDT()).replace(hour=16, minute=0, second=0, microsecond=0) - timedelta(days=365)).timestamp()))}')
+    print(f'365 = {main.get_time_since_stock_data(requested_tickers, int((datetime.now(tz=main.EST5EDT()).replace(hour=16, minute=0, second=0, microsecond=0) - timedelta(days=365)).timestamp()))}')
     end_time = perf_counter()
     print(f'took {end_time - start_time} seconds\n')
 
     # year-to-date
     start_time = perf_counter()
-    print(f'"ytd" = {server_function.get_time_since_stock_data(requested_tickers, "ytd")}')
+    print(f'"ytd" = {main.get_time_since_stock_data(requested_tickers, "ytd")}')
     end_time = perf_counter()
     print(f'took {end_time - start_time} seconds\n')
 
     start_time = perf_counter()
-    print(f'beginning of year = {server_function.get_time_since_stock_data(requested_tickers, int((datetime.now(tz=server_function.EST5EDT()).replace(month=1, day=1, hour=16, minute=0, second=0, microsecond=0)).timestamp()))}')
+    print(f'beginning of year = {main.get_time_since_stock_data(requested_tickers, int((datetime.now(tz=main.EST5EDT()).replace(month=1, day=1, hour=16, minute=0, second=0, microsecond=0)).timestamp()))}')
     end_time = perf_counter()
     print(f'took {end_time - start_time} seconds\n')
 
     # max
     start_time = perf_counter()
-    print(f'"max" = {server_function.get_time_since_stock_data(requested_tickers, "max")}')
+    print(f'"max" = {main.get_time_since_stock_data(requested_tickers, "max")}')
     end_time = perf_counter()
     print(f'took {end_time - start_time} seconds\n')
 
-    # print(json.dumps(server_function.get_daily_stock_data(requested_tickers)))
+    # print(json.dumps(main.get_daily_stock_data(requested_tickers)))
 
     # Thanksgiving market open time (if market was actually open)
-    # print(server_function.get_market_close_time(days_ago=0, from_day=datetime(
+    # print(main.get_market_close_time(days_ago=0, from_day=datetime(
     #     year=2020,
     #     month=11,
     #     day=26,
     #     hour=0,
     #     minute=0,
-    #     tzinfo=server_function.EST5EDT(),
+    #     tzinfo=main.EST5EDT(),
     # )))
-    # print(server_function.get_market_close_time(days_ago=147,))
+    # print(main.get_market_close_time(days_ago=147,))
 
     # Yesterday's market open time
-    # print(server_function.get_market_close_time(days_ago=1,))
-
-
+    # print(main.get_market_close_time(days_ago=1,))
+    
 if __name__ == '__main__':
     test()
